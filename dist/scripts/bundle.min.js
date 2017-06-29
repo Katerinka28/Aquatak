e547f39ed0f8a519a5209a523f0e24e7b2174b52
Katerinka28 <katuxadmitr@gmail.com>
2017-06-29 17:54:08 +0300

    update29/06

---
+++ dist/scripts/bundle.min.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+   //mobile menu 
+    $('.header__mob-icon').click(function(e) {
+        $('.header__mobile').toggleClass('is-mobile-header');
+        $('body').toggleClass('load');
+    });
+    $('.header__mob-close').click(function(e) {
+        $('.header__mobile').removeClass('is-mobile-header');
+        $('body').removeClass('load');
+    });
+});
+
+
+function initMap() {
+        var uluru = {lat: 50.1480734, lng: 30.7377915 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
+
+$(document).ready(function(){
+  $("#menu").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
+$(document).ready(function(){
+  $("#menu-mob").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
+$(document).ready(function(){
+  $("#menu-footer").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
